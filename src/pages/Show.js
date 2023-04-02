baefb787d5e5b74f03194fd0247b0e74a19f7743
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-02 18:26:56 -0400

    added in useParams and useNavigate as well as the find method that checks the id attribute

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,9 +7,24 @@ import { useParams, useNavigate } from "react-router-dom"
 
 
 
-
+// the show component recieves a prop named "people"
 const Show = (props) => {
-    return <h1>Show Component</h1>
+    // return <h1>Show Component</h1>
+    //26 will need useParams and navigate
+    const params = useParams() // useParams is used to retrieve the URL parameters, where id would be the parameter
+    const navigate = useNavigate() // useNavigate is used to navigate to different routes in teh react app
+    const id = params.id 
+    const people = props.people 
+    // person stands for each element in the people array that is being iterated over using the find method
+    // the arrow function checks if the "_id" property of that element is equal to the "id" variable 
+    // if it is, then it is returned and stored in the "person" variable
+    const person = people.find((person) => person._id === id ) 
+
+
+
+
+
+
 }
 
 export default Show
