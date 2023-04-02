c26ac1804efeffc54914e5c814d01a4e58095b39
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-01 21:59:40 -0400

    update the boiler plate in header.js and import the link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,20 @@
 // 3 adding a basic boiler plate for now
+
+
+
+// 8 the link is imported from the router dom and it is used to create links to different pages within the application
+import { Link } from "react-router-dom"
+
+
 const Header = (props) => {
-    return <h1>Header Component</h1>
+    // 9 add the boiler plate, when a user clicks on the link, it will updtae the URL in the browser's adderess bar and render the corresponding component associated with that link
+    return(
+        <nav className='nav'>
+          <Link to="/">
+              <div>People App</div>
+          </Link>
+      </nav>
+    )
 }
 
 export default Header
