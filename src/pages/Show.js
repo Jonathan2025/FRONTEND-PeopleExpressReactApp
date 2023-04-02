7dbfbc7bc6a99dc1c4841ad38c1e3dda84291496
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-02 18:08:08 -0400

    import useState and useparams inot the show.js

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,4 +1,13 @@
 // 3 adding a basic boiler plate for now
+// 24 and 25 import useState and useparams 
+import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom"
+
+
+
+
+
+
 const Show = (props) => {
     return <h1>Show Component</h1>
 }
