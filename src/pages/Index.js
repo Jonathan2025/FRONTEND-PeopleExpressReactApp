d64aa6ca980c1a8efa437f89f70ba1c2a160e6c4
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-02 10:13:45 -0400

    14 and 15, created a function called loaded that accesses the peoples props object and then shows the link to each person in the index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -9,6 +9,30 @@ import { Link } from "react-router-dom"
 
 
 const Index = (props) => {
+
+    // 15 we created a function called loaded that takes no arguments 
+    const loaded = () => {
+        // now we access our "people" props object and then use the MAP function to access each individual person in props
+        // This is in connection with step 14 on Main.js
+        return (props.people.map((person) => (
+            //Now we set a unique identifier called "key" to be the id attribute of that person
+            <div key={person._id} className="person">
+                {/* here we create a link element and set the "to" attribute to be equal to the URL plus the id property of that person */}
+                <Link to={`/people/${person._id}`}>
+                    <h1>{person.name}</h1>
+                </Link>
+                {/* here we create an img element with the src attribute set to the image property of the person object */}
+                <img src={person.image} alt={person.name}/>
+                <h3>{person.title}</h3>
+            </div>
+        )))   
+    }
+
+
+
+
+
+
     return <h1>Index Component</h1>
 }
 
