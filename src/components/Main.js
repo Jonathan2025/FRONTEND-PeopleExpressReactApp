0e4eaf6a7b95550e8e52e51012c55d36e40ffdee
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-01 21:39:25 -0400

    import useEffect and routes, index and header into main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,13 @@
 // 3 adding a basic boiler plate for now
+
+// 6 Import useEffect and routes. In addition import the Index and show pages
+import {useEffect, useState} from "react"
+import {Routes, Route} from "react-router-dom"
+import Index from "../pages/Index"
+import Show from "../pages/Show"
+
+
+
 const Main = (props) => {
     return <h1>Component Name</h1>
 }
