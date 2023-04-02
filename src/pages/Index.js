16d93b4ee0d8e056602e99a20eaad2228e99afe1
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-02 10:04:02 -0400

    imported link and useState into index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,4 +1,13 @@
 // 3 adding a basic boiler plate for now
+
+
+// 14) Import the useState and link from react and routerdom 
+import {useState} from "react"
+import { Link } from "react-router-dom"
+
+
+
+
 const Index = (props) => {
     return <h1>Index Component</h1>
 }
