3d480723bff63840f1cceca07a032c3bec09b52b
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-04-01 21:37:15 -0400

    removed the original boilerplate in app.js and then imported Main and Header components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+//5) import the components 
+import Header from './components/Header';
+import Main from './components/Main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 4) remove the original boiler plate and add in the Header and the Main components */}
+      <Header />
+      <Main />
+    
+    
     </div>
   );
 }
